862e50a67d5baa878d43c4128e3103bfade5b693
lesterao <lesteroropesa@gmail.com>
2014-04-09 10:24:44 -0500

    Debug Out

--- database/mysql.py
+++ database/mysql.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 import MySQLdb
-
 # Establecemos la conexión con la base de datos
 bd = MySQLdb.connect("192.168.80.18","Lester","xxxx","wmomessages" )
- 
 # Preparamos el cursor que nos va a ayudar a realizar las operaciones con la base de datos
 cursor = bd.cursor()
 sql = "SELECT * FROM stationset" 
@@ -22,7 +20,5 @@ try:
       print "Block=%s, StationNumber=%s, Position_Latitud=%d, Position_Longitud=%s" % (Block, StationNumber, Position_Latitud, Position_Longitud)
 except:
    print "Error: No se pudo obtener la data"
- 
-
 # Nos desconectamos de la base de datos
 bd.close()
